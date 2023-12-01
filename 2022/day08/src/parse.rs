ddf9ee5457ba125f6b3f918fbc16b52fd3cb65fb
Alexey Golubev <dr.freecx@gmail.com>
2023-12-01 12:41:06 +0300

    chore: some updates

--- 2022/day08/src/parse.rs
+++ 2022/day08/src/parse.rs
@@ -4,7 +4,7 @@ pub fn parse(input: &str) -> types::Data {
     let mut result = types::Data::default();
     for line in input.lines() {
         let block: Vec<u8> = line.chars().map(|x| x as u8 - b'0').collect();
-        if result.height == 0 {
+        if result.width == 0 {
             result.width = block.len();
         }
         result.data.extend(block);
